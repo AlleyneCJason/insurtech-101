3207db94392048a4c173ad63b035a73a710064a8
AlleyneCJason <jason.alleyne2@aon.com>
2024-08-24 00:41:22 +0000

    st.write("commit")

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building!"
 )
+
+st.title("Login credentials")
+st.write("Credentials determine your product list")
